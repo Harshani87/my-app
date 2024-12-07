7dafac9b25af8195a757d2b62a827a70683826ce
Harshani87 <harshanikanishka666@gmail.com>
2024-12-07 21:07:38 +0530

    update 1 loginpage

---
+++ app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-yellow-200 text-center">
+      <div className='bg-blue-100 px-5 py-3 rounded-lg'>Login Page</div>
+      
+      
+      </div>
+  )
+}
